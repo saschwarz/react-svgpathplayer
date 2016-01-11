0c8129fc9da1bd8f86327eb7b2781ed84c5bb669
Steve Schwarz <steve@agilitynerd.com>
2016-01-10 19:48:03 -0600

    fix(demo): Corrected naming of dist files used in demo script.js

--- demo/script.js
+++ demo/script.js
@@ -1,7 +1,7 @@
 require('./images/segments.svg');
 require('./images/pathsegments.svg');
-require('../dist/svgpathplayer.min.js');
-require('../dist/svgpathplayer.min.js.map');
-require('../dist/svgpathplayer.js');
-require('../dist/svgpathplayer.js.map');
+require('../dist/react-svgpathplayer.min.js');
+require('../dist/react-svgpathplayer.min.js.map');
+require('../dist/react-svgpathplayer.js');
+require('../dist/react-svgpathplayer.js.map');
 require('../src/svgpathplayer.jsx');
